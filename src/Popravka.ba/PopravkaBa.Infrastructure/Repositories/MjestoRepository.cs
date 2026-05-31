af620d43a08d4a92e4fbf777ac16d899f9dd5dfa
astaffz <mustaficaid@gmail.com>
2026-05-31 23:39:32 +0200

    fix: Ispravlja višak EF navigation property u MjestoRepository

--- src/Popravka.ba/PopravkaBa.Infrastructure/Repositories/MjestoRepository.cs
+++ src/Popravka.ba/PopravkaBa.Infrastructure/Repositories/MjestoRepository.cs
@@ -17,30 +17,25 @@ namespace PopravkaBa.Infrastructure.Repositories
 
         public async Task<IEnumerable<Mjesto>> DajSvaMjestaAsync()
             => await _context.Mjesta
-                    .Include(m => m.Kanton)
                     .ToListAsync();
 
         public async Task<IEnumerable<Mjesto>?> DajMjestaPoKantonuAsync(int kantonID)
             => await _context.Mjesta
                     .Where(m => m.Kanton.Equals(kantonID))
-                    .Include(m => m.Kanton)
                     .ToListAsync();
         public async Task<Mjesto?> DajPoIdAsync(int id)
             => await _context.Mjesta
-                .Include(m => m.Kanton)
                 .FirstOrDefaultAsync(m => m.MjestoID == id);
                 
 
         public async Task<IEnumerable<Mjesto>> PronadjiMjestaAsync(string pretraga)
             => await _context.Mjesta
-                 .Include(m => m.Kanton)
                   .ToListAsync();
 
         public async Task<IEnumerable<Mjesto>> DajSveKorisnikeMjestaAsync(string korisnikId)
             => await _context.KorisnikMjesto
                 .Where(km => km.KorisnikID == korisnikId)
                 .Select(km => km.Mjesto)
-                .Include(m => m.Kanton)
                 .ToListAsync();
 
         public async Task DodajMjestaKorisniku(string korisnikId, List<int> mjestaID)
