3eb458dfbc1d498fad2422735c4622b50ed3a10e
astaffz <mustaficaid@gmail.com>
2026-06-05 20:55:35 +0200

    feat: Dodaje redirect guard na registraciji za već prijavljene korisnike

---
+++ src/Popravka.ba/PopravkaBa.Web/Attributes/RedirectIfAuthenticatedAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace PopravkaBa.Web.Attributes
+{
+    public class RedirectIfAuthenticatedAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.User.Identity?.IsAuthenticated == true)
+                context.Result = new RedirectToActionResult("Index", "Home", null);
+        }
+    }
+}
