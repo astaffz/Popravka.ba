4e95eb2cd65e3f10ba071f6f61f5f751fb3d2d81
astaffz <mustaficaid@gmail.com>
2026-06-06 00:49:05 +0200

    feat: Dodaje nemapirane kolone za UI u AppUser i Firma

--- src/Popravka.ba/PopravkaBa.Domain/Models/ApplicationUser.cs
+++ src/Popravka.ba/PopravkaBa.Domain/Models/ApplicationUser.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using PopravkaBa.Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PopravkaBa.Domain.Models
 {
 
     public class ApplicationUser : IdentityUser
     {
+        [NotMapped]
+        public virtual string DisplayName => $"{Ime} {Prezime}" ?? UserName;
+
+        [NotMapped]
+        public virtual string SkracenoIme => !string.IsNullOrEmpty(Prezime) ? $"{Ime} {Prezime[0]}." : DisplayName;
+
+        public virtual Status StatusVerifikacije => EmailConfirmed ? Status.Aktivan : Status.NaCekanju;
         public string? Ime { get; set; }
         public string? Prezime  { get; set; }
         public DateTime DatumRegistracije { get; set; } = DateTime.UtcNow;
--- src/Popravka.ba/PopravkaBa.Domain/Models/Firma.cs
+++ src/Popravka.ba/PopravkaBa.Domain/Models/Firma.cs
@@ -1,19 +1,27 @@
 ﻿
 using Microsoft.AspNetCore.Identity;
 using PopravkaBa.Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PopravkaBa.Domain.Models
 {
     public class Firma : IzvrsilacUsluge
     {
-
+        [NotMapped]
+        public override string DisplayName => NazivFirme;
+        [NotMapped]
+        public override string SkracenoIme => NazivFirme;
+     
+       
         public string NazivFirme { get; set; }
         public int MinZaposlenih { get; set; } = 1;
         public int MaxZaposlenih { get; set; } = 5;
         private string? JIB { get; set; }
-        public Status StatusVerifikacije { get; set; } = Status.NaCekanju;
+        public bool AdminVerificirao { get; set; } = false; 
+        public override Status StatusVerifikacije =>
+            EmailConfirmed && AdminVerificirao ? Status.Aktivan : Status.NaCekanju;
 
-       // TODO Migration1: Cuvati informacije o satima u bazi, radnovrijeme za format i  kao enum u Firma.
+        // TODO Migration1: Cuvati informacije o satima u bazi, radnovrijeme za format i  kao enum u Firma.
         public TimeSpan? OtvorenoOd { get; set; }
         public TimeSpan? OtvorenoDo { get; set; }
         public VelicinaFirme VelicinaFirme { get; set; } = VelicinaFirme.Mikro;
